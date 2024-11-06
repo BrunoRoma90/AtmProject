9b937fdeda3ac3bfc4a4e43d87cec4fe19984d53
BrunoRoma90 <brunoroma.90@gmail.com>
2024-11-06 18:26:38 +0000

    Include Models,Services,Repositories

---
+++ AtmServices/Interfaces/IBankAccountServices.cs
@@ -0,0 +1,20 @@
+﻿using AtmModels;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtmServices.Interfaces
+{
+    public interface IBankAccountServices
+    {
+        public List<BankAccount> GetAllBankAccounts();
+
+        public BankAccount GetBankAccountById(int id);
+        public List<BankAccount> GetBankAccountByUserId(int userId);
+
+        public Boolean UpdateBalanceBankAccount(BankAccount bankAccountUpdated);
+    }
+}
